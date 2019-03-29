59c2fa217dd3a9796ca10ed9170f62e11538b5a7
Ruslan Koldunsky <kodlunsky@gmail.com>
2019-03-29 11:01:19 +0300

    Initial Commit

---
+++ kixie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  kixie
+//
+//  Created by Ruslan Koldunsky on 29/03/2019.
+//  Copyright © 2019 Ruslan Koldunsky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ kixie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  kixie
+//
+//  Created by Ruslan Koldunsky on 29/03/2019.
+//  Copyright © 2019 Ruslan Koldunsky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
